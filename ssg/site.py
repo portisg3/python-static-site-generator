695da6b99564661fd0329dd4e934c97f1e0533f2
portisg3 <gretaportis@northwesternmutual.com>
2022-02-16 14:21:43 -0600

    module1

--- ssg/site.py
+++ ssg/site.py
@@ -4,9 +4,9 @@ class Site:
     def __init__(self,source,dest):
         self.source = Path(source)
         self.dest = Path(dest)
-        
+
     def create_dir(self,path):
-        directory = self.dest/path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
